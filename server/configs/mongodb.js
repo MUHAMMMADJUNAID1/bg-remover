c2fdb3238e3615d17f42d61e74bc46baeb484dca
unknown <muhammadjunaid9394@gmail.com>
2024-10-20 23:14:31 +0500

    working on the db connection

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
 
 const connectMongoDb = async () => {
-  mongoose.connection.on("Connected", () => {
-    console.log("Database connected!");
-  });
-  await mongoose.connect(`${process.env.MONGO_DB_URI}/bg-removal`);
+  try {
+    mongoose.connection.on("Connected", () => {
+      console.log("Database connected!");
+    });
+    await mongoose.connect(`${process.env.MONGO_DB_URI}/bg-removal`);
+  } catch (error) {
+    console.error("MONGODB connection error: ", error);
+    process.exit(1);
+  }
 };
 
 export default connectMongoDb;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import dotenv from "dotenv";
 import connectMongoDb from "./configs/mongodb.js";
 
+dotenv.config({ path: "./.env" });
 // app config
-
 const PORT = process.env.PORT || 4000;
-
 // initalize instatnce of express
-
 const app = express();
 // connecting mongodb
 await connectMongoDb();
@@ -17,11 +16,17 @@ await connectMongoDb();
 app.use(express.json());
 app.use(cors());
 
+connectMongoDb()
+  .then(() => {
+    app.listen(process.env.PORT || 4000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 4000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB  connection error !!!", err);
+  });
+
 // api routes
 app.get("/", (req, res) => {
   return res.send("Api working");
-});
-
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
 });

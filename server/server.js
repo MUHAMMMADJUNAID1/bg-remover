ab41a1c7e06d9063ade9fa9c0151a82d42b2a4fb
unknown <muhammadjunaid9394@gmail.com>
2024-10-22 09:03:24 +0500

    Solving the issue faced in production

--- server/server.js
+++ server/server.js
@@ -1,3 +1,33 @@
+// import express from "express";
+// import "dotenv/config";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import connectMongoDb from "./configs/mongodb.js";
+
+// dotenv.config({ path: "./.env" });
+// // app config
+// const PORT = process.env.PORT || 4000;
+// // initalize instatnce of express
+// const app = express();
+// // initalize middleware
+// app.use(express.json());
+// app.use(cors());
+// console.log(process.env.MONGO_DB_URI);
+
+// connectMongoDb()
+//   .then(() => {
+//     app.listen(process.env.PORT || 4000, () => {
+//       console.log(`Server is running on port ${process.env.PORT || 4000}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("MONGODB  connection error !!!", err);
+//   });
+
+// // api routes
+// app.get("/", (req, res) => {
+//   return res.send("Api working");
+// });
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
@@ -7,10 +37,9 @@ import connectMongoDb from "./configs/mongodb.js";
 dotenv.config({ path: "./.env" });
 // app config
 const PORT = process.env.PORT || 4000;
-// initalize instatnce of express
+// initialize instance of express
 const app = express();
-
-// initalize middleware
+// initialize middleware
 app.use(express.json());
 app.use(cors());
 console.log(process.env.MONGO_DB_URI);
@@ -22,10 +51,12 @@ connectMongoDb()
     });
   })
   .catch((err) => {
-    console.log("MONGODB  connection error !!!", err);
+    console.log("MONGODB connection error !!!", err);
   });
 
 // api routes
 app.get("/", (req, res) => {
   return res.send("Api working");
 });
+
+export default app; // Export the 'app' instance
